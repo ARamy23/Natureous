f7db85167d807035a88d4aed9a90aa18fbde759b
Ahmed Ramy <dev.ahmedramy@icloud.com>
2020-04-17 15:18:31 +0200

    Setup basic foundation for a follow player system

---
+++ Assets/Natureous/Characters/PathFindingAgent/PathFindingAgent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Natureous
+{
+    public class PathFindingAgent : MonoBehaviour
+    {
+        public bool ShouldTargetPlayableCharacter;
+        public GameObject Target;
+        NavMeshAgent navMeshAgent;
+
+        private void Awake()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        public void GoToTarget()
+        {
+            if (ShouldTargetPlayableCharacter)
+            {
+                Target = CharacterManager.Instance.GetPlayableCharacter().gameObject;
+            }
+
+            navMeshAgent.SetDestination(Target.transform.position);
+        }
+    }
+}
+
+
---
+++ Assets/Natureous/Utilities/LayerAdder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Natureous
+{
+    public enum NatureousLayer
+    {
+        PostProcessing,
+        Character,
+        Bot,
+        Ground,
+    }
+
+    public class LayerAdder : MonoBehaviour
+    {
+
+    }
+}
