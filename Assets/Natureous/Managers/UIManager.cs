3607d16e67145a1ae8f1e68fee0cdb369400bba2
Ahmed Ramy <dev.ahmedramy@icloud.com>
2020-04-19 17:32:01 +0200

    Implement Fader

---
+++ Assets/Natureous/Managers/UIManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Natureous
+{
+    public class UIManager : Singleton<UIManager>
+    {
+        public float ScreenAlpha;
+    }
+}
+
+
---
+++ Assets/Natureous/Scenes/Scenes Utilities/SceneFader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Natureous
+{
+    public class SceneFader : MonoBehaviour
+    {
+
+        CanvasGroup canvasGroup;
+
+        private void Start()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        IEnumerator FadeOutIn()
+        {
+            yield return FadeOut(1f);
+            yield return FadeIn(2f);
+        }
+
+        public IEnumerator FadeOut(float duration)
+        {
+            while (canvasGroup.alpha < 1)
+            {
+                canvasGroup.alpha += Time.deltaTime / duration;
+                yield return null;
+            }
+        }
+
+        public IEnumerator FadeIn(float duration)
+        {
+            while (canvasGroup.alpha > 0)
+            {
+                canvasGroup.alpha -= Time.deltaTime / duration;
+                yield return null;
+            }
+        }
+    }
+}
+
+
